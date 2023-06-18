c0817a60a5ab8182711c8896ffbc1917ddaca051
Guilherme Potter Petry <guipotterrp@gmail.com>
2023-06-18 20:31:23 -0300

    aula Construtor Tear-Off concluída

---
+++ lib/22_constructor_tearoff/1_constructor_tearoff.dart
@@ -0,0 +1,29 @@
+void main() {
+  final nomes = ['Guilherme', 'Potter', 'Petry'];
+
+  final pessoasAntigo = nomes.map((nome) => Pessoa(nome)).toList();
+
+  //final pessoas = nomes.map<Pessoa>(Pessoa.nome).toList();
+  //final pessoas = nomes.map<Pessoa>(Pessoa.new).toList();
+  final pessoas = nomes.map<Pessoa>(Pessoa.fromJson).toList();
+  for (var pessoa in pessoas) {
+    print('Olá ${pessoa.nome}');
+  }
+
+  funcaoQualquer(Pessoa.new);
+  
+}
+
+void funcaoQualquer(Pessoa Function(String) funcao) {
+  funcao('Guilherme');
+}
+
+class Pessoa {
+  String nome;
+
+  Pessoa(this.nome);
+  Pessoa.nome(this.nome);
+  factory Pessoa.fromJson(String nome) {
+    return Pessoa(nome);
+  }
+}
